c96e9b5a988e27cb2c2b5c83505a275c17813f73
Toshiki <a5816094@aoyama.jp>
2018-07-23 17:19:45 +0900

    change

--- qfromw/urls.py
+++ qfromw/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^$', views.appmain, name='appmain'),
-  url(r'^input/$', views.input_word, name='input_word'),
+  # url(r'^$', views.appmain, name='appmain'),
+  url(r'^$', views.input_word, name='input_word'),
 ]
--- qfromw/views.py
+++ qfromw/views.py
@@ -13,55 +13,25 @@ def input_word(request):
         'input': request.GET.get('word'),
     }
 
-    #入力した文字
+    #入力した文字　
     tango = str(d.get('input'))
-    print(tango)
+    #print(tango)
     res = requests.get('https://ejje.weblio.jp/content/'+tango)
     soup = bs4.BeautifulSoup(res.text, "html5lib")
 
+    onsei = soup.select('i.fa.fa-volume-up.contentTopAudioIcon source')
+    print('音声', onsei)
+    mp3 = re.findall('https://.*mp3' , str(onsei))
+    print('mp3', mp3)
+
     try:
         imi = soup.select('div.summaryM.descriptionWrp td.content-explanation')[0].getText()
+
         d['explanation'] = imi
     except:
         e = {
             'error': "その単語は存在しません"
         }
         return render(request, 'input.html',e)
+
     return render(request, 'input.html',d)
-
-def appmain(request):
-
-    form = inputForm()
-    #if request.method == 'POST':
-
-
-    word = 'What'
-    res = requests.get('https://ejje.weblio.jp/content/'+word)
-    soup = bs4.BeautifulSoup(res.text, "html5lib")
-
-    try:
-        explanation = soup.select('div.summaryM.descriptionWrp td.content-explanation')[0].getText()
-        #print(explanation)
-    except:
-        print('そのページは存在しない')
-    # この Wiki エントリのタイトルの文字列を変数 title に代入
-    #title = soup.select("#firstHeading")[0].getText()
-
-
-    # この Wiki エントリのトップにある説明文だけ取り出して変数 description に代入
-    #description = soup.select('div.mw-content-ltr p')[0].getText()
-    # ( ) 内に答えにつながる言葉があるエントリが多いので，( ) は取り除いてしまう．最短マッチが重要
-    #description2 = re.sub(r"（.*?）|(.*?)", ' ', description)
-
-    # 答えに当たる部分を ◯ で置き換える．文字数が同じなのはヒントのため
-    #description3 = description2.replace(title, '◯' * len(title))
-
-    # 答えが置き換わったときだけクイズにする
-    #if description2 != description3:
-    #    break
-
-    # demo/main.hml に値を渡す
-    return render(request, 'demo/main.html', {
-        'answer' : word,
-        'descr' : explanation,
-    })
